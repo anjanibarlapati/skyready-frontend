1ce5636fa569e9452188491929b8475703992978
anoosha-sanugula <goldie@Anoosha-Sanugula.local>
2025-07-11 12:25:39 +0530

    Add tests for footer component

---
+++ src/pages/footer/Footer.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react'
+import { Footer } from './Footer'
+import { describe, test, expect } from 'vitest'
+
+describe('Footer', () => {
+  test('renders footer text', () => {
+    render(<Footer />)
+    expect(screen.getByText(/© 2025 SkyReady. All rights reserved. ✈️/i)).toBeTruthy();
+  })
+})
