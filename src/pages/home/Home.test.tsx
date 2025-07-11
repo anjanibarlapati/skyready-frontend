e799384eff64fb701624b20ce02260e0aa1340eb
anoosha-sanugula <goldie@Anoosha-Sanugula.local>
2025-07-11 12:27:04 +0530

    Add tests for homepage

---
+++ src/pages/home/Home.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react'
+import { Home } from './Home'
+import { describe, test, expect } from 'vitest'
+
+describe('Home', () => {
+  test('renders footer in home', () => {
+    render(<Home />)
+    expect(screen.getByText(/© 2025 SkyReady. All rights reserved. ✈️/i)).toBeTruthy();
+  })
+})
