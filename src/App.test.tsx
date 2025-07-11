7858777dafcbdb2974e69b383f67371b0c0321bc
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-07-11 11:44:40 +0530

    Add tests for welcome message

---
+++ src/App.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react'
+import App from './App'
+import { describe, test, expect } from 'vitest'
+
+describe('App', () => {
+  test('renders welcome message', () => {
+    render(<App />)
+    expect(screen.getByText(/Welcome to SkyReady/i)).toBeTruthy();
+  })
+})
