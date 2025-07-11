5300b53743112a748ccebab863424782dd2f2183
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-07-11 17:59:40 +0530

    Add image slider component in home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,11 @@
 import { AboutUs } from "../../components/AboutUs/AboutUs"
+import { ImageSlider } from "../../components/ImageSlider/ImageSlider"
 import "./Home.css"
 
 export const Home = () => {
   return (
     <div className="home-container" data-testid="home">
+      <ImageSlider/>
       <AboutUs/>
     </div>
   )
