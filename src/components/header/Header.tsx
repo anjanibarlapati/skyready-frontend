db4d4eccafc291d400883cf5d9fbd266500ea6fa
SoumyaKatta <soumya.katta@everest.engineering>
2025-07-11 14:49:58 +0530

    Add header section with logo image for SkyReady app

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,13 @@
 export const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div>
+      <header className="header">
+        <div className="header-container">
+          <div className="logo-group">
+            <img src="/logo.png" alt="SkyReady Logo" className="logo-img" />
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
