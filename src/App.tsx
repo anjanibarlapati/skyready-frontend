7cf11607b6a6fd47ebdcedd3efe174bbead5d677
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-07-11 17:11:32 +0530

    Add header and footer in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
+import { Footer } from './components/footer/Footer'
+import { Header } from './components/header/Header'
 import { Home } from './pages/home/Home'
 
 export function App() {
   return (
     <div className='app-container'>
+      <Header/>
       <Home/>
+      <Footer/>
     </div>
   )
 }
