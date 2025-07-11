f15606037217e7ab8cd8dcbfcd91cce74a4f29de
SoumyaKatta <soumya.katta@everest.engineering>
2025-07-11 15:13:27 +0530

    Render header component in Home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,10 @@
 import { Footer } from "../../components/footer/Footer"
+import { Header } from "../../components/header/Header"
 import "./Home.css"
 export const Home = () => {
   return (
     <div className="home-container">
+      <Header/>
       <Footer/>
     </div>
   )
