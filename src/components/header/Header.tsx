76a579331134b043d76c277720195a6e524b6e08
SoumyaKatta <soumya.katta@everest.engineering>
2025-07-11 14:55:53 +0530

    Add navigation bar with Home link and about us link styled as button

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,6 +6,14 @@ export const Header = () => {
           <div className="logo-group">
             <img src="/logo.png" alt="SkyReady Logo" className="logo-img" />
           </div>
+          <nav className="nav-links">
+            <a href="/" className="nav-button">
+              Home
+            </a>
+            <a href="#about-us" className="nav-button">
+              About Us
+            </a>
+          </nav>
         </div>
       </header>
     </div>
