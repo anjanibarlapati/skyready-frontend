01976c29bf0e8a8acc41b35d5b4c8c55ac969cc4
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-07-11 17:13:37 +0530

    Implement about us component with styling

---
+++ src/components/AboutUs/AboutUs.tsx
@@ -0,0 +1,18 @@
+import './AboutUs.css'
+
+export const AboutUs = ()=>{
+    return(
+        <div className='about-us-container' id="about-us">
+            <section className="about-us">
+                <div className="about-container">
+                    <h2>About Us</h2>
+                    <p>
+                    SkyReady is a simple and reliable flight booking platform designed to make travel planning easy.
+                    Our goal is to help users find the right flights quickly, with transparent prices and a smooth booking experience.
+                    Whether it’s a one-way trip or a round journey, SkyReady is here to get you ready for takeoff — in just one tap. ✈️
+                    </p>
+                </div>
+            </section>
+        </div>
+    )
+}
