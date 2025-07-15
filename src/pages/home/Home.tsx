0963f0b207ee87c56eb56f66c4a95e0bb40e4c54
durgamkeerthana <keerthana.durgam@everest.engineering>
2025-07-15 18:30:04 +0530

    Add flight results card in home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,30 @@
+import { useSelector } from "react-redux";
 import { AboutUs } from "../../components/AboutUs/AboutUs";
 import { ImageSlider } from "../../components/ImageSlider/ImageSlider";
 import { Search } from "../../components/search/Search";
 import "./Home.css";
+import type { RootState } from "../../redux/store";
+import { FlightResult } from "../../components/flight_result/FlightResult";
 
 export const Home = () => {
+  const { flights, message } = useSelector((state: RootState) => state.flights);
+
   return (
     <div className="home-container" data-testid="home">
       <ImageSlider/>
       <Search />
+      { ( flights.length > 0 || message ) && <div className="flight-results-container">
+        {flights.length > 0 ? (
+          <>
+            <h2>Available Flights</h2>
+            {flights.map((flight, index) => (
+              <FlightResult key={index} flight={flight} />
+            ))}
+          </>
+        ) : (
+          <p>{message}</p>
+        )}
+      </div>}
       <AboutUs/>
     </div>
   );
