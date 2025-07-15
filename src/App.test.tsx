f835234103bbb41ed1ea46aff6e6daf23d339593
durgamkeerthana <keerthana.durgam@everest.engineering>
2025-07-15 18:32:14 +0530

    Update test to to render the app wrapped in provider

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,16 @@
 import { render, screen } from '@testing-library/react'
 import { describe, expect, test } from 'vitest'
 import { App } from './App'
+import { store } from './redux/store'
+import { Provider } from 'react-redux'
 
 describe('App', () => {
   test('renders home footer', () => {
-    render(<App />)
+    render(
+      <Provider store={store}>
+         <App />
+      </Provider>
+    )
     expect(screen.getByText(/© 2025 SkyReady. All rights reserved. ✈️/i)).toBeInTheDocument();
   })
 })
