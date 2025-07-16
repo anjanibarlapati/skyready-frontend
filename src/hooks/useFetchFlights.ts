091e70a08d4dcaec1e80a0e08a54a9b43ca417c3
SoumyaKatta <soumya.katta@everest.engineering>
2025-07-16 21:30:38 +0530

    Create a useFetchFlight custom component for fetching available flights

---
+++ src/hooks/useFetchFlights.ts
@@ -0,0 +1,41 @@
+
+import { useDispatch } from "react-redux";
+import { setFlights, setMessage, type SearchData } from "../redux/flightsSlice";
+
+export const useFetchFlights = () => {
+  const dispatch = useDispatch();
+
+  const fetchFlights = async (searchData: SearchData) => {
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_BASE_URL}/api/v1/flights/search`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            source: searchData.source,
+            destination: searchData.destination,
+            departure_date: searchData.selectedDate,
+            travellers_count: searchData.travellersCount,
+            class_type: searchData.classType,
+          }),
+        }
+      );
+
+      const result = await response.json();
+
+      if (response.ok) {
+        dispatch(setFlights(result.flights));
+      } else {
+        dispatch(setFlights([]));
+        dispatch(setMessage(result.message || "Failed to fetch flights."));
+      }
+    } catch {
+      dispatch(setMessage("Something went wrong while fetching flights."));
+    }
+  };
+
+  return { fetchFlights };
+};
