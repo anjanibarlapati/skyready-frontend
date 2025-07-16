2d720740d542b0d404f176c0ca979f00b3a09145
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-07-16 13:05:22 +0530

    Add routes in App with initial path and confirm booking page path

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Footer } from './components/footer/Footer'
 import { Header } from './components/header/Header'
 import { Home } from './pages/home/Home'
+import { ConfirmBooking } from './pages/confirmBooking/ConfirmBooking'
 
 export function App() {
   return (
     <div className='app-container'>
       <Header/>
-      <Home/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/confirm-booking" element={<ConfirmBooking />} />
+        </Routes>
       <Footer/>
     </div>
   )
